06f377f73491e8df6836dbc1ec77fecebc74889c
Renzo Castro <renzogcastro@bcp.com.pe>
2022-09-21 12:45:07 -0500

    feat: improve base code

--- app.js
+++ app.js
@@ -2,13 +2,53 @@
 
 // [START gae_node_request_example]
 const express = require('express');
-
 const app = express();
+const mime = require('mime');
+const fs = require('fs')
 
-app.use('/.well-known', express.static('static/.well-known'))
+app.all('/app/*', function(req, res, next) 
+{
+  // Set respons header (geen idee of dit compleet is)
+  res.header("Access-Control-Allow-Origin","*");
+  res.header("Access-Control-Allow-Methods","GET");
+  res.header("Access-Control-Allow-Headers","X-Requested-With,Content-type,Accept,X-Access-Token,X-Key");
+
+  // Set response contenttype
+  // res.contentType('application/json');
+
+  next();
+});
+
+// app.use('/.well-known', express.static('static/.well-known'))
+
+function sendFile(res, fileURI, contentType) {
+  var file = './static/' + fileURI;
+
+  if (!fs.existsSync(file)) {
+    file = './static/404.html';
+  }
+
+  fs.readFile(file, function(err, data) {
+    if(err) {
+      res.send("Oops! Couldn't find that file.");
+    } else {
+      contentType = contentType || mime.lookup(file) || 'text/plain';
+
+      // set the content type based on the file
+      res.setHeader('content-type', contentType);
+      res.contentType(contentType);
+      res.send(data);
+    }
+    res.end();
+  });
+}
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname +  '/stsatic/index.html');
+  sendFile(res, 'index.html');
+});
+
+app.get('/:file', (req, res) => {
+  sendFile(res, req.params.file);
 });
 
 // Start the server
